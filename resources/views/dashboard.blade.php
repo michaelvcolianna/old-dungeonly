a3791cbaeeacbe8cdb9b33ec711dc80cf63089c5
MVC <michael.colianna@me.com>
2021-06-26 04:31:12 -0400

    Tweaks the dashboard display - Places a coming soon message - Replaces the application mark with the d10

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="p-6 sm:px-20 bg-white">
+                    <div class="text-2xl">
+                        Welcome to Dungeonly!
+                    </div>
+
+                    <div class="mt-6 text-gray-500">
+                        There will be more here later.
+                    </div>
+                </div>
             </div>
         </div>
     </div>
