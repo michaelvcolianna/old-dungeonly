b7cf686a362d2d3c90e98f204123256e11d98f7e
MVC <michael.colianna@me.com>
2021-07-09 12:07:50 -0400

    Cleans up quick jump - Removes unneeded entanglement - Removes unecessary updateSelected argument - Simplies HTML structure - Organizes element classes - Utilizes element.dataset on element-selected dispatcher - Removes unecessary dataset element

--- resources/views/livewire/shadowrun/quick-jump.blade.php
+++ resources/views/livewire/shadowrun/quick-jump.blade.php
@@ -1,8 +1,8 @@
 <div class="w-full md:w-1/3">
     <div
+        class="relative"
         x-data="{
             open: @entangle('show_results'),
-            search: @entangle('search'),
             selected: @entangle('selected'),
             selectedIndex: 0,
             selectPrevious()
@@ -28,79 +28,80 @@
                     behavior: 'smooth'
                 });
             },
-            updateSelected(id, key, label)
+            updateSelected(id, key)
             {
                 this.open = false;
-                this.search = label;
                 this.selected = key;
                 this.selectedIndex = 0;
 
                 scrollAndAccountForHeader(key);
             },
         }"
+        x-on:element-selected="
+            updateSelected($event.detail.id, $event.detail.key)
+        "
     >
-        <div
-            class="relative"
-            x-on:element-selected="updateSelected($event.detail.id, $event.detail.key, $event.detail.label)"
-        >
-            <span>
-                <div class="relative">
-                    <x-jet-input
-                        class="block mt-4 md:mt-0 w-full quick-jump placeholder-gray-400 md:bg-transparent md:placeholder-transparent"
-                        type="text" placeholder="Quick jump to section"
-                        wire:model.debounce.300ms="search"
-                        x-on:keydown.arrow-down.stop.prevent="selectNext()"
-                        x-on:keydown.arrow-up.stop.prevent="selectPrevious()"
-                        x-on:keydown.enter.stop.prevent="$dispatch('element-selected', {
-                            id: $refs.results.children[selectedIndex].getAttribute('data-result-id'),
-                            key: $refs.results.children[selectedIndex].getAttribute('data-result-key'),
-                            label: $refs.results.children[selectedIndex].getAttribute('data-result-label')
-                        })"
-                    />
-                    <span
-                        class="absolute top-0 left-0 h-full w-full items-center text-transparent -z-1 hidden md:flex"
-                    >
-                        Quick jump to section (ESC to focus)
-                    </span>
-                </div>
-            </span>
+        <div class="relative">
+            <x-jet-input
+                class="
+                    quick-jump block mt-4 placeholder-gray-400 w-full
+                    md:bg-transparent md:mt-0 md:placeholder-transparent
+                "
+                type="text" placeholder="Quick jump to section"
+                wire:model.debounce.300ms="search"
+                x-on:keydown.arrow-down.stop.prevent="selectNext()"
+                x-on:keydown.arrow-up.stop.prevent="selectPrevious()"
+                x-on:keydown.enter.stop.prevent="$dispatch('element-selected', {
+                    id: $refs.results.children[selectedIndex].dataset.resultId,
+                    key: $refs.results.children[selectedIndex].dataset.resultKey
+                })"
+            />
 
-            <div
-                class="absolute bg-white left-0 right-0 mt-2"
-                x-show="open" x-on:click.away="open = false"
+            <span
+                class="
+                    absolute h-full hidden items-center left-0 text-transparent
+                    top-0 w-full -z-1 md:flex
+                "
             >
-                <ul
-                    class="{{ $results ? 'border' : '' }} border-gray-300 flex flex-row flex-wrap md:block"
-                    x-ref="results"
-                >
-                    @foreach($results as $index => $result)
-                        <li
-                            wire:key="jump-result-{{ $loop->index }}"
-                            x-on:click.stop="$dispatch('element-selected', {
-                                id: {{ $loop->index }},
-                                key: '{{ $index }}',
-                                label: '{{ $result['label'] }}'
-                            })"
-                            :class="{
-                                'bg-gray-600': {{ $loop->index }} === selectedIndex,
-                                'text-white': {{ $loop->index }} === selectedIndex,
-                                'p-4': true
-                            }"
-                            data-result-id="{{ $loop->index }}"
-                            data-result-key="{{ $index }}"
-                            data-result-label="{{ $result['label'] }}"
-                        >
-                            <span>
-                                {{ $result['label'] }}
+                Quick jump to section (ESC to focus)
+            </span>
+        </div>
 
-                                @if($result['group'])
-                                    <small>({{ $result['group'] }})</small>
-                                @endif
-                            </span>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
+        <div
+            class="absolute bg-white left-0 mt-2 right-0"
+            x-show="open" x-on:click.away="open = false"
+        >
+            <ul
+                class="border-gray-300 flex flex-row flex-wrap md:block"
+                :class="{
+                    'border': {{ $results ? 'true' : 'false' }}
+                }"
+                x-ref="results"
+            >
+                @foreach($results as $index => $result)
+                    <li
+                        class="p-4" wire:key="jump-result-{{ $loop->index }}"
+                        :class="{
+                            'bg-gray-600': {{ $loop->index }} === selectedIndex,
+                            'text-white': {{ $loop->index }} === selectedIndex
+                        }"
+                        x-on:click.stop="$dispatch('element-selected', {
+                            id: {{ $loop->index }},
+                            key: '{{ $index }}'
+                        })"
+                        data-result-id="{{ $loop->index }}"
+                        data-result-key="{{ $index }}"
+                    >
+                        <span>
+                            {{ $result['label'] }}
+
+                            @if($result['group'])
+                                <small>({{ $result['group'] }})</small>
+                            @endif
+                        </span>
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 </div>
